a24e4183799fd4c17272e6475a2811745d52da0e
Ahmed <ahmed_l2@yahoo.com>
2024-07-31 03:07:10 +0300

    5. Mock logging in

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 """Basic flask app"""
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 
 class Config:
@@ -25,10 +33,22 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
+def get_user():
+    """Returns user information from the mock database"""
+    user_id = request.args.get('login_as', type=int)
+    return users.get(user_id)
+
+
+@app.before_request
+def before_request():
+    """Set user information as global before each request"""
+    g.user = get_user()
+
+
 @app.route('/')
 def hello():
     """Route to index"""
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == '__main__':
